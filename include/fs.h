cb643860f41639bb0243cbd5d24c8a7e9ca354ff
Miguel Bernabeu Diaz <miguelbernadi@gmail.com>
2011-04-18 00:10:40 +0200

    Updated fs.h to working usage

--- include/fs.h
+++ include/fs.h
@@ -2,23 +2,43 @@
 #include <string>
 #include <stdexcept>
 
+// Add path parsing abilities to distinguish /home/usr/ and foo.txt,
+// recreate paths and abstract from platform dependent paths.
+class FileDescriptor
+{
+  private:
+    std::string filename, path;
+
+  public:
+    FileDescriptor(std::string str) : filename(str), path("") { }
+    FileDescriptor(std::string filename, std::string path) : filename(filename), path(path) { }
+    FileDescriptor(std::string& filename, std::string& path) : filename(filename), path(path) { }
+    std::string filePath() const { return this->path + this->filename; }
+    std::string fileName() const { return this->filename; }
+};
+
+// Be careful with loose streams (changing from read to write streams several times 
+// may create lots of fstreams that are not taken care of. Be careful.
 class File
 {
   private:
-    std::string filename;
-    std::fstream* fstream;
-    bool _fileExists();
+    FileDescriptor* filed;
+    std::ifstream* fstream;
+
   public:
     File();
-    File(std::string const & filename) : filename(filename) { }
-    std::string& getFilename() const;
-    void setFilename(std::string const& filename);
-    void read();
+    File(std::string& filename) : filed( new FileDescriptor(filename) ) { } 
+    std::string getFilename() const { this->filed->filePath(); }
+    void setFilename(std::string& filename);
+    bool _fileExists();
+    std::ifstream* getIStream() { this->_fileExists(); return this->fstream; }
+//    std::ofstream getOStream() const { this->fstream = new std::ofstream( this->filed->filePath().c_str() ); return *this->fstream; }
+//    std::fstream& getFStream() { this->fstream = new std::fstream( this->filed->filePath().c_str() ); return *this->fstream; }
 };
 
 bool File::_fileExists()
 {
-    this->fstream = new std::ifstream( filename.c_str() );
+    this->fstream = new std::ifstream( this->filed->filePath().c_str() );
     if( !fstream )
     {
 //        throw 
@@ -30,23 +50,8 @@ bool File::_fileExists()
     }
 }
 
-std::string& File::getFilename() const
+void File::setFilename(std::string& str) 
 {
-    return this->filename;
+    this->filed = new FileDescriptor( str );
 }
-
-void File::setFilename(std::string const& str) 
-{
-    this->filename = str;
-}
-
-/*
-void File::read()
-{
-    this->_fileExists();
-    while( fstream >> i )
-    {
-    {    
-}
-*/
 
